7682354b55bfe2e9d01b56cd0b8571ec4ed9b043
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2018-01-16 22:28:27 +0100

    Week 2 - assignemnt - gradient descent

--- week2/machine-learning-ex1/ex1/gradientDescent.m
+++ week2/machine-learning-ex1/ex1/gradientDescent.m
@@ -15,19 +15,10 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    theta = theta - (alpha * X' * ((X * theta) - y) / length(y));
 
-end
-
-end
+endfor
