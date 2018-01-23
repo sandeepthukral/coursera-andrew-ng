4b9f026ba9d189fe7f7ec152a82b088d0e8637ac
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2018-01-23 21:54:44 +0100

    week4 - implemented forward propogation

--- week4/ex3/predict.m
+++ week4/ex3/predict.m
@@ -21,7 +21,20 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add x0=1 as the first column
+X = [ones(size(X,1),1) X];
 
+% a2 = g(Theta1*a1) %% here a1 = X
+A2 = sigmoid(X * Theta1');
+
+% add a2_0=1 as the first column
+A2 = [ones(size(A2,1),1) A2];
+
+% h0 = g(Theta2*a2)
+h0 = sigmoid(A2 * Theta2');
+
+% p will be the index of the max of every row
+[minval, p] = max(h0, [], 2);
 
 
 
