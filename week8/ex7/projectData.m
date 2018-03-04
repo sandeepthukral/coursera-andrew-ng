a94a5cdfe083c3e74789d08b4630e5e0175da31c
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2018-03-04 23:00:57 +0100

    implemented projectData

--- week8/ex7/projectData.m
+++ week8/ex7/projectData.m
@@ -23,4 +23,12 @@ Z = zeros(size(X, 1), K);
 
 % =============================================================
 
+for i=1:size(X, 1),
+    for j=1:K,
+        x = X(i, :)';
+        projection_k = x' * U(:, j);
+        Z(i, j) = projection_k;
+    end
+end
+
 end
